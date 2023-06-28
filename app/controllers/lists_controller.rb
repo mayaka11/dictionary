4207798d4b0cb016ef37ecca105f9bfa038d12a5
EC2 Default User <mayoneko0@gmail.com>
2023-06-28 12:52:02 +0000

    投稿、一覧、詳細、編集、削除の作成完了

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,47 @@
+class ListsController < ApplicationController
+  def new
+    @list = List.new #Viewへ渡すためのインスタンス変数にからのModelオブジェクトを生成する。
+  end
+
+  def create
+    list = List.new(list_params) #データを受け取り新規登録するためのインスタンス作成
+    list.save #データをデータベースに保存するためのsaveメソッド実行
+    redirect_to list_path(list.id) #詳細ページへリダイレクト
+  end
+
+  def index
+    @lists = List.all #テーブルに保存されたデータを全取得する
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id]) #データを1件取得
+    list.update(list_params) #データのアップデート
+    redirect_to list_path(list.id) #詳細ページへリダイレクト
+  end
+
+  def destroy
+    list = List.find(params[:id]) #データを１件取得
+    list.destroy #データを削除
+    redirect_to '/lists' #投稿一覧画面へリダイレクト
+  end
+
+
+
+  private
+  #ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+    #params => formから送られてくるデータはparamsの中に入ります。
+    #require => 送られてきたデータの中からモデル名（ここでは:list）を指定し、データを絞り込む。
+    #permit => requireで絞り込んだデータの中から、保存を許可するカラムを指定します。
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  get '/top' => 'homes#top'
+  root 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #get 'lists/new'
+  #post 'lists' => 'lists#create'
+  #get 'lists' => 'lists#index'
+  #get 'lists/:id' => 'lists#show', as: 'list' #ルーティング（名前付きルート）
+  #get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  #patch 'lists/:id' => 'lists#update', as: 'update_list'
+  #delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+
+  resources :lists #上記のlistのルートをひとまとめにする
 end
